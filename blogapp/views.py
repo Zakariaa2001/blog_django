402602135e0f21818114868e8b6138d5e9257445
zakaria <91892819+Zakariaa2001@users.noreply.github.com>
2024-02-22 16:46:38 +0100

    first commit

---
+++ blogapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def course_list(request):
+    return
+
+def course_form(request):
+    return
+
+def course_delete(request):
+    return
